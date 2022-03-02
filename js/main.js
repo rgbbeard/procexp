f0c0d255c40955f5f4fa5bfc2c7b23afb675c112
rgb_beard <62804472+rgbbeard@users.noreply.github.com>
2022-03-02 17:26:00 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,76 @@
+window.onload = function() {
+	let
+		timeout = 119,
+		display = document.getElementById("refresh-display"),
+		scrolltop = document.getElementById("scrolltop"),
+		scrolldown = document.getElementById("scrolldown"),
+		rows = document.querySelectorAll("table tbody tr"),
+		search = document.querySelector('input[name="search"]'),
+		search_process = document.querySelector('input[name="search-process"]'),
+		filter_vital = document.querySelector('input[name="filter_vital"]'),
+		filter_daemon = document.querySelector('input[name="filter_daemon"]'),
+		filter_system = document.querySelector('input[name="filter_system"]');
+
+	rows.forEach(r => {
+		r.onclick = function() {
+			rows.forEach(i => i.classList.remove("selected"));
+
+			r.classList.add("selected");
+		};
+	});
+
+	setInterval(function() {
+		display.textContent = timeout;
+		timeout--;
+	}, 1000);
+
+	setTimeout(function() {
+		window.location.href = "";
+	}, timeout*1000);
+
+	scrolltop.onclick = function() {
+		window.scrollTo(0, 0);
+	};
+
+	scrolldown.onclick = function() {
+		window.scrollTo(0, document.documentElement.scrollHeight);
+	};
+
+	search.onclick = function(e) {
+		if(e == undefined || e == null) {
+			e = window.event;
+		}
+
+		e.preventDefault();
+
+		let params = [];
+
+		if(search_process.value.trim() !== "") {
+			params.push("pn=" + btoa(search_process.value));
+		}
+
+		if(filter_vital.checked || filter_daemon.checked || filter_system.checked) {
+			let filters = [];
+
+			if(filter_vital.checked) {
+				filters.push(filter_vital.value);
+			}
+
+			if(filter_daemon.checked) {
+				filters.push(filter_daemon.value);
+			}
+
+			if(filter_system.checked) {
+				filters.push(filter_system.value);
+			}
+
+			filters = btoa(filters.join(";"));
+		}
+
+		params = params.join("&");
+
+		if(params.trim() !== "") {
+			window.location.href = "http://localhost:9010/taskmanager.php?" + params;
+		}
+	};
+};
